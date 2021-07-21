a4d5719f8e8901a319b470ce30b0f26d816a69d0
Jd Beecham <TheBeeVirus@users.noreply.github.com>
2021-07-20 17:40:53 -0700

    Altered Typed Text

--- src/Interface/About.js
+++ src/Interface/About.js
@@ -8,9 +8,10 @@ const About = () => {
                 <h1>Hello, I am Jd Beecham.</h1>
                 <Typed 
                     className="typed-text"
-                    strings = {["iOS Developer", "Frontend Developer", "Backend Developer"]}
+                    strings = {["I am a iOS Developer.", "I am a Frontend Developer.", "I am a Backend Developer.", "I am a Game Developer.", "I am a Softare Tester."]}
                     typeSpeed={40}
                     backSpeed={60}
+                    smartBackspace
                     loop
                 /> 
 
