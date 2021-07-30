46398eb7d66f282248b43f70177188049c8f8ad6
Jd Beecham <TheBeeVirus@users.noreply.github.com>
2021-07-29 23:14:24 -0700

    Started work on Work section

--- src/Interface/Work.js
+++ src/Interface/Work.js
@@ -4,9 +4,38 @@ import './CSS/Work.css';
 const Work = () => {
     return (
         <section id="work" className="d-flex min-vh-100">
-            <div className="container py-2">
-                <div id="pageHeaderTitle" className="h1 text-center text-light">Todo: Work</div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="main-timeline">
+                <div class="timeline">
+                    <a href="/#" class="timeline-content">
+                        <span class="timeline-year">2020-2021</span>
+                        <div class="timeline-icon">
+                            <i class="fa fa-globe"></i>
+                        </div>
+                        <h3 class="title">Web Designing</h3>
+                        <p class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer males uada tellus lorem, et condimentum neque commodo
+                        </p>
+                    </a>
+                </div>
+                <div class="timeline">
+                    <a href="/#" class="timeline-content">
+                        <span class="timeline-year">2019</span>
+                        <div class="timeline-icon">
+                            <i class="fa fa-rocket"></i>
+                        </div>
+                        <h3 class="title">Web Development</h3>
+                        <p class="description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer males uada tellus lorem, et condimentum neque commodo
+                        </p>
+                    </a>
+                </div>
             </div>
+        </div>
+    </div>
+</div>
         </section>
     )
 }
