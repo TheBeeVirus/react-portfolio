1cda79059c7a82161b3d75d4be560cf5521bbbfb
Jd Beecham <TheBeeVirus@users.noreply.github.com>
2021-07-30 19:38:53 -0700

    Working on the Work Section

--- src/Interface/Work.js
+++ src/Interface/Work.js
@@ -5,20 +5,24 @@ const Work = () => {
     return (
         <section id="work" className="d-flex min-vh-100">
             <div class="container">
+                <div className="h1 text-center text-light" id="pageHeaderTitle">Work History</div>
                 <div class="row">
                     <div class="col-md-12">
                         <div class="main-timeline">
                             <div class="timeline">
-                                <a href="/#" class="timeline-content">
-                                    <span class="timeline-year">2020-2021</span>
+                                <div class="timeline-content">
+                                    <span class="timeline-year">2019-2020</span>
                                     <div class="timeline-icon">
-                                        <i class="fa fa-globe"></i>
+                                        <i class="fas fa-tools"></i>
                                     </div>
-                                    <h3 class="title">Web Designing</h3>
+                                    <h3 class="title">Field Service Technician / Walman Instruments</h3>
                                     <p class="description">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer males uada tellus lorem, et condimentum neque commodo
+                                    Responsible for installing, providing maintenance, troubleshooting and repairs to eye doctors and surgeons in
+                                    Washington, Oregon, Idaho and Montana. Entrusted with a company vehicle and credit card. Maintain
+                                    monthly travel and expense reports. Helped customers budget repairs and plan out maintenance
+                                    services to keep costs low.
                                     </p>
-                                </a>
+                                </div>
                             </div>
                             <div class="timeline">
                                 <a href="/#" class="timeline-content">
