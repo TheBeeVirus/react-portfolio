6d94fe6489069bf1ac6e1113b3161f1034d56349
Jd Beecham <TheBeeVirus@users.noreply.github.com>
2021-07-31 22:55:38 -0700

    Working on Work section, altered a few CSS values.

--- src/Interface/Work.js
+++ src/Interface/Work.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './CSS/Work.css';
+import WorkHistory from '../Variables/WorkHistory'
 
 const Work = () => {
     return (
@@ -9,33 +10,20 @@ const Work = () => {
                 <div class="row">
                     <div class="col-md-12">
                         <div class="main-timeline">
-                            <div class="timeline">
-                                <div class="timeline-content">
-                                    <span class="timeline-year">2019-2020</span>
-                                    <div class="timeline-icon">
-                                        <i class="fas fa-tools"></i>
+                            {WorkHistory.map( job => 
+                                <div class="timeline">
+                                    <div class="timeline-content">
+                                        <span class="timeline-year">{job.year}</span>
+                                        <div class="timeline-icon">
+                                            <i class={job.icon}></i>
+                                        </div>
+                                        <h3 class="title">{job.title}</h3>
+                                        <p class="description">
+                                        {job.description}
+                                        </p>
                                     </div>
-                                    <h3 class="title">Field Service Technician / Walman Instruments</h3>
-                                    <p class="description">
-                                    Responsible for installing, providing maintenance, troubleshooting and repairs to eye doctors and surgeons in
-                                    Washington, Oregon, Idaho and Montana. Entrusted with a company vehicle and credit card. Maintain
-                                    monthly travel and expense reports. Helped customers budget repairs and plan out maintenance
-                                    services to keep costs low.
-                                    </p>
                                 </div>
-                            </div>
-                            <div class="timeline">
-                                <a href="/#" class="timeline-content">
-                                    <span class="timeline-year">2019</span>
-                                    <div class="timeline-icon">
-                                        <i class="fa fa-rocket"></i>
-                                    </div>
-                                    <h3 class="title">Web Development</h3>
-                                    <p class="description">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer males uada tellus lorem, et condimentum neque commodo
-                                    </p>
-                                </a>
-                            </div>
+                            )}
                         </div>
                     </div>
                 </div>
---
+++ src/Variables/WorkHistory.js
@@ -0,0 +1,29 @@
+
+const WorkHistory = [
+    {
+        year: "2019 - 2020",
+        icon: "fas fa-tools",
+        title: "Field Service Technician / Walman Instruments",
+        description: "Responsible for installing, providing maintenance, troubleshooting and repairs to eye doctors and surgeons in Washington, Oregon, Idaho and Montana. Entrusted with a company vehicle and credit card. Maintain monthly travel and expense reports. Helped customers budget repairs and plan out maintenance services to keep costs low."
+    },
+    {
+        year: "2001 - 2019",
+        icon: "fas fa-flask",
+        title: "Certified Lead Lab Technician / LensCrafters",
+        description: "Responsible for crafting prescription lenses to fit in a frame. Troubleshooted and repaired all the equipment in the lab and doctor’s office. I was responsible for the lab’s budget and ordering all the supplies. Wrote schedules and manage other technicians. I trained new employees and lab managers for multiple locations. Entrusted by the regional manager to travel to labs in the region to install, troubleshoot and train techs on operating and maintenance of new equipment. My lab was consistently ranked within the top 10 in the company out of over 1000 labs, which is based off of quality of work and workload."
+    },
+    {
+        year: "2006 & 2011",
+        icon: "fas fa-laptop-code",
+        title: "Freelance Web Developer",
+        description: "Worked during my free time as a web developer, building small websites for individuals. Would design the site in Photoshop. I would then break it down into parts and build a site using PHP as the back-ends and then build the front-end with HTML, CSS and JavaScript."
+    },
+    {
+        year: "2008 - 2009",
+        icon: "fas fa-bug",
+        title: "Software Tester / Foundation 9 Entertainment",
+        description: "Analyzed a range of different software titles on a variety of different systems including PC, Xbox, Sony PSP, PlayStation, Nintendo DS, and Wii. Worked on a prototype breadboard for a children’s doll that interacted with a computer game. Along with testing titles, had to write technical documents including test cases, bug reports, and design suggestions. All documents had to be well written, clear but detailed to allow others to be able to reproduce and understandable without questions. Had the opportunity to work on several different databases and backend systems because we had to adapt to customer’s owned systems. Received commendation from a senior programmer because of how helpful and knowledgeable I was for him during a Dora Explorer project. "
+    }
+];
+
+export default WorkHistory
