beb5c56d0cfa167ca86afe2b683a24a61244854a
TheBeeVirus <TheBeeVirus@users.noreply.github.com>
2021-08-02 10:52:01 -0700

    Tweaks to CSS in About, Contact, and Social Bar.

--- src/Interface/Contact.js
+++ src/Interface/Contact.js
@@ -7,10 +7,11 @@ const Contact = () => {
             <div className="container py-2">
                 <div class="row">
                     <div id="pageHeaderTitle" className="h1 text-center text-light">How to Contact Me?</div>
-                    <h1 className="col-12 fs-4 p-5">Removed my form because of Bots, until I get time to add a bot checker, you can feel free to contact me through LinkedIn, Twitter, or GitHub.</h1>
-                    <a href="https://www.linkedin.com/pub/jd-beecham/5b/44/506" className="col-4 text-center fs-1" target="_blank" rel="noreferrer"><i className="fab fa-linkedin"></i></a>
-                    <a href="https://twitter.com/TheBeeVirus" className="col-4 text-center fs-1" target="_blank" rel="noreferrer"><i className="fab fa-twitter"></i></a>
-                    <a href="https://github.com/TheBeeVirus" className="col-4 text-center fs-1" target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a> 
+                    <h1 className="col-12 fs-4 p-5 text-center">You can feel free to contact me through LinkedIn, Twitter, GitHub or Discord.</h1>
+                    <a href="https://www.linkedin.com/pub/jd-beecham/5b/44/506" className="col-3 text-center fs-1" target="_blank" rel="noreferrer"><i className="fab fa-linkedin"></i></a>
+                    <a href="https://twitter.com/TheBeeVirus" className="col-3 text-center fs-1" target="_blank" rel="noreferrer"><i className="fab fa-twitter"></i></a>
+                    <a href="https://github.com/TheBeeVirus" className="col-3 text-center fs-1" target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a>
+                    <a href="https://discordapp.com/users/TheBeeVirus#9177/" className="col-3 text-center fs-1" target="_blank" rel="noreferrer"><i class="fab fa-discord"></i></a> 
                 </div>
             </div>
         </section>
