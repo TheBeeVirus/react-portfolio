d9b5e0172f70875bce24e747e4a2cc66ed90b519
TheBeeVirus <TheBeeVirus@users.noreply.github.com>
2021-09-29 12:05:18 -0700

    Updated comments

--- public/scroller.js
+++ public/scroller.js
@@ -1,13 +1,26 @@
+// Simple script to tweak side scrolling cards.
+// www.thedigitalhive.com
+// Note: to fix the smoothness of the scroll, I used CSS
+//    .scrollbox {
+//          scroll-behavior: smooth;
+//    }
+
+// Check if page is loaded
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
-    // document ready
+
+    // Save our targeted scrolling elements
     const scrollbox = document.querySelector('.scrollbox');
+    // Save the size of each card, so we know how much to shift over by
     var card_size = scrollbox.querySelector('div').clientWidth;
 
+    // Fix glitch when window resizes, so does card width, so
+    // need to update width.
     function WindowResized() {
       card_size = scrollbox.querySelector('div').clientWidth;
     }
 
+    // When Window size changes, call update function.
     window.onresize = WindowResized;
 
     function scrollToNextCard() {
@@ -18,9 +31,12 @@ document.onreadystatechange = () => {
       scrollbox.scrollBy(-card_size, 0);
     }
 
+    // Set listeners for click, and call Scroll Functions
     document.querySelector('.btn.next').addEventListener('click', scrollToNextCard);
     document.querySelector('.btn.prev').addEventListener('click', scrollToPrevCard);
 
+    // Set listener for mouse wheel, stop default scroll up/down
+    // then call side to side card scroll functions.
     scrollbox.addEventListener("wheel", function (e) {
 
         e.preventDefault();
